f7da746c00231d891daeaa0e1ee6e8a5683cb877
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-10-01 14:50:45 +0530

    styled login form

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLoginWithGoogle, startLoginWithGithub } from "../actions/auth";
 
 export const LoginPage = ({ startLoginWithGoogle, startLoginWithGithub }) => (
-  <div>
-    <h1>Login</h1>
-    <button onClick={startLoginWithGoogle}>Login with google</button>
-    <button onClick={startLoginWithGithub}>Login with Github</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Pocket-Money</h1>
+      <p className="box-layout__paragraph">Start saving today! A rupee at a time.</p>
+      <button className="button" onClick={startLoginWithGoogle}>
+        Sign in with <span style={{ color: "red" }}>Google</span>
+      </button>
+      <button className="button" onClick={startLoginWithGithub}>
+        Sign in with <span style={{ color: "#838383" }}>Github</span>
+      </button>
+    </div>
   </div>
 );
 
