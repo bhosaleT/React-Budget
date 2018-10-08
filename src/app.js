85f339eda77ef7257e0bacdb8a7169218e94d058
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-10-08 17:23:15 +0530

    added a loading screen

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import "./styles/styles.scss";
 import AppRouter, { history } from "./routers/AppRouter";
 import "react-dates/lib/css/_datepicker.css";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -31,7 +32,7 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
+ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
 firebase.auth().onAuthStateChanged(user => {
   if (user) {
